2c0efcc04a393575425aeec0d8dbca2d36145f0a
Prajot24 <gpkp168030@gmail.com>
2024-02-20 11:15:50 +0530

    Second

--- companyapi/api/serializers.py
+++ companyapi/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import *
 
+
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Company
-        fields = "__all__"
+        fields = '__all__'
+        include = ['company_id']
--- companyapi/api/views.py
+++ companyapi/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
 # Create your views here.
+
+class CompanyViewset(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
